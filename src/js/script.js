25fcfb8c3eda1bf42cf0552dc6150684564fc1e0
Dorota Tomala <doforever@gmail.com>
2020-12-09 14:44:54 +0100

    Add to favorite

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,20 @@
 {
   'use strict';
+  /* Global variables */
+  const favoriteBooks = []; // Stores ids of favorite books
+
+  /* Listners */
+  function initActions(){
+    const covers = document.querySelectorAll('.book__image');
+    for (let cover of covers){
+      /* 'dblclick' starts addToFavorite */
+      cover.addEventListener('dblclick', function(){
+        addToFavorite(this);
+      });
+    }
+  }
+
+  /* Functions */
 
   function renderBooks(){
     const bookTemplate = Handlebars.compile(document.querySelector('#template-book').innerHTML);
@@ -10,5 +25,16 @@
     }
   }
 
+  function addToFavorite(target){
+    /* add data-id to favoriteBooks */
+    if (!favoriteBooks.includes(target.dataset.id)) favoriteBooks.push(target.dataset.id);
+    /* add class favorite to .book__image */
+    target.classList.add('favorite');
+    console.log('favorites', favoriteBooks);
+
+  }
+
+  /* Run app */
   renderBooks();
+  initActions();
 }
