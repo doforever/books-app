6f904e2054ad686f7f92a218f231c64808de5688
Dorota Tomala <doforever@gmail.com>
2020-12-10 11:53:46 +0100

    Filter books

--- src/js/script.js
+++ src/js/script.js
@@ -2,15 +2,31 @@
   'use strict';
   /* Global variables */
   const favoriteBooks = []; // Stores ids of favorite books
+  const filters = []; // Stores chosen filters
+
+  /* DOM references */
+  const filtersWrapper = document.querySelector('.filters');
+  const booksList = document.querySelector('.books-list');
 
   /* Listners */
   function initActions(){
     /* 'dblclick' starts addToFavorite */
-    const booksList = document.querySelector('.books-list');
     booksList.addEventListener('dblclick', function(event){
       if (event.target.offsetParent.classList.contains('book__image')){
         addToFavorite(event.target.offsetParent);
       }
+    });
+    /* 'click' on checkbox updates filters and filter books */
+    filtersWrapper.addEventListener('click', function(event){
+      if (event.target.tagName === 'INPUT' && event.target.type === 'checkbox'
+      && event.target.name === 'filter'){
+        if (event.target.checked){
+          filters.push(event.target.value);
+        } else if (filters.includes(event.target.value)){
+          filters.splice(filters.indexOf(event.target.value),1);
+        }
+      }
+      filter();
     });
   }
 
@@ -18,7 +34,6 @@
 
   function renderBooks(){
     const bookTemplate = Handlebars.compile(document.querySelector('#template-book').innerHTML);
-    const booksList = document.querySelector('.books-list');
     for (let book of dataSource.books){
       const generatedHTML = bookTemplate(book);
       booksList.appendChild(utils.createDOMFromHTML(generatedHTML));
@@ -32,6 +47,29 @@
     } else {
       favoriteBooks.splice(favoriteBooks.indexOf(target.dataset.id), 1);
       target.classList.remove('favorite');
+    }
+  }
+
+  function filter(){
+    console.log('filters', filters);
+    const adultFilter = filters.includes('adults') ? 'adults' : '';
+    const fictionFilter = filters.includes('nonFiction') ? 'nonFiction' : '';
+    function checkFilter (filter, book) {
+      if (filter && book.details[filter]) {
+        return true;
+      } else if (!filter && !book.details[filter]){
+        return true;
+      }
+    }
+    /* loop through all books dataSource.books */
+    for (let book of dataSource.books){
+      const bookDOM = booksList.querySelector(`a[data-id="${book.id}"]`);
+      if (!checkFilter(adultFilter, book) || !checkFilter(fictionFilter, book)){
+      /* add or remove class hidden */
+        bookDOM.classList.add('hidden');
+      } else {
+        if (bookDOM.classList.contains('hidden')) bookDOM.classList.remove('hidden');
+      }
     }
   }
 
